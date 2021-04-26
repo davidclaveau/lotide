14efbb5a5abd04f2f3128e5bd1de6f8e2cdaa042
David Claveau <daveclaveau@gmail.com>
2021-04-26 21:26:01 +0000

    Added some template literals to assertEqual and added the function to head.js; head.js returns the 0th value in the array.

--- head.js
+++ head.js
@@ -8,8 +8,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
+const head = function(arr) {
+  return arr[0];
+};
+
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual(1, 2);
-assertEqual("Yes", "Yes");
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head(["Hello"]), "Hello");
+assertEqual(head([]), "Hello");
