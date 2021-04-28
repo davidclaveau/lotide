c841742f0d5cc4e0a5a3787adc6d61f11ff87052
David Claveau <daveclaveau@gmail.com>
2021-04-28 00:01:24 +0000

    Small change to one of the tests on the code which compared array length.

--- eqArrays.js
+++ eqArrays.js
@@ -23,7 +23,7 @@ const eqArrays = function(arrayOne, arrayTwo) {
   return true;
 };
 
-eqArrays([1, 2, 3], [1, 2, 3, 4]); // => true
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
 eqArrays([1, 2, 3], [3, 2, 1]); // => false
 
 eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
