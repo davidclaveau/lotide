57cd46130e4adbc4cc63c0bb6a662df5742d1a51
David Claveau <daveclaveau@gmail.com>
2021-04-29 23:17:40 +0000

    Changed how flatten.js works, no longer 'recursion' because it wasn't quite correct. Completes flattening of arrays for single-nested arrays.

--- flatten.js
+++ flatten.js
@@ -1,60 +1,45 @@
 const assertArraysEqual = function(actual, expected) {
-  let truthCount = 0;
-  for (let val = 0; val < actual.length; val++) {
-    if (actual[val] !== expected[val]) {
-      // Return false if any of the values don't perfectly match
-      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-      break;
-    } else {
-      truthCount++;
-    }
-  }
-
-  if (truthCount === expected.length) {
-    // truthCount only equals expected.length if every value matched perfectly
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else if (truthCount !== expected.length) {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
+  console.log(eqArrays(actual, expected));
 };
 
 const eqArrays = function(arrayOne, arrayTwo) {
+
+  const pass = `✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`;
+  const fail = `🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`;
+
+
   // If the lengths are different, they don't match and can end the code
   if (arrayOne.length !== arrayTwo.length) {
-    return false;
+    return fail;
   }
   // Return false if any of the values don't perfectly match
   for (let val = 0; val < arrayOne.length; val++) {
     if (arrayOne[val] !== arrayTwo[val]) {
-      return false;
+      return fail;
     }
   }
   // If able to exit the loop/if statement, all values match and return true
-  return true;
+  return pass;
 };
 
-const flatten = function(values, arr) {
-  let flattenedArr = [];
-
-  if (arr) {
-    // If arr exists, pass values to flattenedArr
-    flattenedArr = arr;
-  }
+const flatten = function(values) {
+  const flattenedArr = [];
 
   for (value of values) {
-    // If this is a nested array, recall the function and pass current flattenedArr
-    // Else, push the number to flattenedArr
-    Array.isArray(value) ? flatten(value, flattenedArr) : flattenedArr.push(value);
+    if (Array.isArray(value)) {
+      for (innerValue of value) {
+        flattenedArr.push(innerValue);
+      }
+    } else {
+      flattenedArr.push(value)
+    }
   }
   return flattenedArr;
 };
 
 
 console.log(flatten([1, 2, [3, 4], 5, [6]])); // => [1, 2, 3, 4, 5, 6]
+
+// TEST
 assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => PASS
-
-// Test recursion
-console.log(flatten([1, 2, [3, 4, [5, 6]], 7, [8]])); // => [1, 2, 3, 4, 5, 6]
-
-console.log(flatten([1, 2, [3, 4, [5, 6, [7, 8, 9]]], 10, [11]])); // => PASS
-assertArraysEqual(flatten([1, 2, [3, 4, [5, 6, [7, 8, 9]]], 10, [11]]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]); // => PASS
+assertArraysEqual(flatten([1, 2, [3, 4], [5, 6], [7, 8, 9], 10, [11]]), [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]); // => PASS
