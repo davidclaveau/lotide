248619143b08eba4febbd7008d2656739414377c
David Claveau <daveclaveau@gmail.com>
2021-05-03 21:55:28 +0000

    Made changes to the asserArraysEquals file and added a test for it. Also added the assertEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,29 +1,7 @@
+const eqArrays = require('./eqArrays');
+
 const assertArraysEqual = function(actual, expected) {
   console.log(eqArrays(actual, expected));
 };
 
-const eqArrays = function(arrayOne, arrayTwo) {
-
-  const pass = `✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`;
-  const fail = `🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`;
-
-
-  // If the lengths are different, they don't match and can end the code
-  if (arrayOne.length !== arrayTwo.length) {
-    return fail;
-  }
-  // Return false if any of the values don't perfectly match
-  for (let val = 0; val < arrayOne.length; val++) {
-    if (arrayOne[val] !== arrayTwo[val]) {
-      return fail;
-    }
-  }
-  // If able to exit the loop/if statement, all values match and return true
-  return pass;
-};
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); // => true
-assertArraysEqual([1, 2, 3], [3, 2, 1]); // => false
-
-assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => true
-assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => false
+module.exports = assertArraysEqual;
